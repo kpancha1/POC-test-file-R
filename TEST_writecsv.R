5bfbaab2a34d53e542ea24c6b571930e0bc96ed9
Keya Panchal <96100375+kpancha1@users.noreply.github.com>
2023-08-11 11:45:43 -0700

    Update TEST_writecsv.R

--- TEST_writecsv.R
+++ TEST_writecsv.R
@@ -13,6 +13,6 @@ file_path <- file.path(path, csvfilename)
 write.csv(data, file = file_path, row.names = FALSE)
 
 return_datetime <- function() {
-  return(datetime)
+  return(cat(datetime))
 }
 return_datetime()
